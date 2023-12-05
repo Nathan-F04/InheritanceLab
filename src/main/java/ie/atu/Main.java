99e5fa8609dfe99163994687fa86da111ecf7249
G00413711@atu.ie <G00413711@atu.ie>
2023-12-05 13:29:01 +0000

    Added getters/setters constructors to person and completed part a

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Person myPerson=new Person("Test","Test2","Test3");
+       System.out.println(myPerson.toString());
+    }
+}
---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,55 @@
+package ie.atu;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Person {
+
+
+       private String name,address,telNumber;
+    //Constructors
+    public Person() {
+        this.name = "John";
+        this.address = "Galway";
+        this.telNumber = "0851119443";
+    }
+
+    public Person(String name, String address, String telNumber) {
+        this.name = name;
+        this.address=address;
+        this.telNumber=telNumber;
+    }
+    //Getters and Setters
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", telNumber='" + telNumber + '\'' +
+                '}';
+    }
+}
